6d10b908f3f705e3175ef4d264078daa69fe6494
Ingrid G <ing.gape18@gmail.com>
2023-10-02 18:43:12 -0500

    without success page

--- form.js
+++ form.js
@@ -1,11 +1,12 @@
-/* import isValidEmail from './utils/validations.js'
- */export const handleSubmit = (event) => {
+import { isValidEmail } from "./utils/validations";
+
+export const handleSubmit = (event) => {
     event.preventDefault();
   
     const myForm = event.target;
     const formData = new FormData(myForm);
 
-/* //---
+//---
     const name = formData.get("name");
     const email = formData.get("email");
   
@@ -17,7 +18,7 @@
     if (!isValidEmail(email)) {
       alert("Por favor, ingresa un email válido.");
       return;
-    } */
+    }
     
     myForm.reset();
 //---
@@ -27,7 +28,7 @@
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     })
-        .then(() => navigate("/success.html")) // custom page insted of console.log
-        //.then(() => console.log("Form successfully submitted"))
+        //.then(() => navigate("/success.html")) // custom page insted of console.log
+        .then(() => console.log("Form successfully submitted"))
         .catch((error) => alert(error));
   };
